ad82ecb22a29110cc0e952de8ca7026bbac93dc9
Gugu1127 <80884134+Gugu1127@users.noreply.github.com>
2022-08-01 02:28:42 +0800

    Add files via upload

--- src/avoidance.cpp
+++ src/avoidance.cpp
@@ -26,6 +26,10 @@ class Group {
     Group() {
         index_group.clear();
         index_group.push_back(move(pair<int, int>(0, 9)));
+        index_group.push_back(move(pair<int, int>(10, 19)));
+        index_group.push_back(move(pair<int, int>(20, 29)));
+        index_group.push_back(move(pair<int, int>(691, 700)));
+        index_group.push_back(move(pair<int, int>(701, 710)));
         index_group.push_back(move(pair<int, int>(711, 719)));
         sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &Group::scanCallback, this);
         pub = n.advertise<std_msgs::String>("/collision", 1);
@@ -33,7 +37,6 @@ class Group {
 
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
         vector<double> average;
-        bool collision = false;
         for (int i = 0; i < index_group.size(); i++) {
             double sum = 0;
             int counter = 0;
@@ -46,21 +49,20 @@ class Group {
                 average.push_back(0);
             else
                 average.push_back(sum / (index_group[i].second - index_group[i].first - counter + 1));
-            cout << *(average.end() - 1) << endl;
         }
 
+        int num_of_collision = 0;
         for (int i = 0; i < average.size(); i++) {
-            if (average[i] <= 0.2) {
-                collision = true;
-                break;
+            if (average[i] <= 0.3) {
+                num_of_collision++;
             }
         }
 
         std_msgs::String msg;
-        if (collision)
+        msg.data = "false";
+        if (num_of_collision >= 2)
             msg.data = "true";
-        else
-            msg.data = "false";
+            
         pub.publish(msg);
     }
 };
@@ -68,6 +70,7 @@ class Group {
 int main(int argc, char** argv) {
     ros::init(argc, argv, "avoidance");
     Group group;
+
     ros::spin();
 
     return 0;
