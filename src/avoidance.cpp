e33221c3d85f8c6b9a281eea02ce698ba3a8a1c1
jason89923 <jason89923@gmail.com>
2022-08-11 13:59:40 +0800

    filter update and analyze

--- src/avoidance.cpp
+++ src/avoidance.cpp
@@ -6,9 +6,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+
 using namespace std;
 
-#define default_window_size 31
+#define default_window_size 1
 #define obstacle_size 5
 #define top_distance_stop 0.2
 #define top_distance_slow 0.6
@@ -18,7 +20,7 @@ using namespace std;
 #define bottom_corner_distance_stop 0.20
 
 int window_size = default_window_size ;
-
+int counter = 0 ;
 
 
 enum Position {
@@ -91,23 +93,59 @@ class Group {
             window_size = temp;
         }
 
-        cout << "Current window size: " << window_size << endl;
+        //cout << "Current window size: " << window_size << endl;
     }
 
-    void filter(vector<double>& array, const vector<int>& index_array, const sensor_msgs::LaserScan::ConstPtr& scan) {
-        array.clear();
+    void filter(vector<double>& average_array, const sensor_msgs::LaserScan::ConstPtr& scan) {
+        average_array.clear();
         int half_window_size = floor(window_size / 2);
-        for (int i = 0; i < index_array.size(); i++) {
-            if (i - half_window_size >= 0 && i + half_window_size < index_array.size()) {
-                double no_zero_elements = 0, average = 0;
-                for (int j = i - half_window_size, k = 0; k < window_size; j++, k++) {
-                    average += scan->ranges[index_array[j]];
-                    if (scan->ranges[index_array[j]] > 0) {
-                        no_zero_elements++;
-                    }
+        for (int i = 0; i < 720 ; i++) {
+            double total = 0, no_zero_elements = 0;
+            for (int j = i - half_window_size; j <= i + half_window_size ; j++) {
+                int index = j;
+                if ( j < 0) {
+                    index += 720;
+                } else if (j >= 720) {
+                    index -= 720;
                 }
 
-                array.push_back(no_zero_elements == 0 ? 0 : average / no_zero_elements);
+                total += scan->ranges[index] ;
+                if (scan->ranges[index] > 0) {
+                    no_zero_elements++;
+                }
+            }
+
+            average_array.push_back(no_zero_elements == 0 ? 0 : total / no_zero_elements);
+        }
+
+        if (counter == 100) {
+            ROS_INFO("current window size: %d", window_size);
+            window_size += 2;
+            cout << endl;
+            counter = 0;
+        }
+
+        if (counter == 0) {
+            cout << "window_size = " << window_size << ", ";
+        }
+
+        int num_of_zeros = 0;
+        for (int i = 0 ; i < average_array.size() ; i++) {
+            if (average_array[i] == 0) {
+                num_of_zeros++ ;   
+            }
+        }
+
+
+        cout << num_of_zeros << ", ";
+
+        counter += 1;
+    }
+
+    void distribute(vector<double> array[6], const vector<int> index_array[6], const vector<double>& average_array) {
+        for (int i = 0 ; i < 6 ; i++) {
+            for (int j = 0 ; j < index_array[i].size() ; j++) {
+                array[i].push_back(average_array[index_array[i][j]]);
             }
         }
     }
@@ -149,15 +187,14 @@ class Group {
             }
         }
 
-
         return 1;
     }
 
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
+        vector<double> filter_array;
+        filter(filter_array, scan);
         vector<double> arrayList[6];
-        for (int i = 0 ; i < 6 ; i++) {
-            filter(arrayList[i], index_group[i], scan);
-        }
+        distribute(arrayList, index_group, filter_array);
 
         std_msgs::Int16MultiArray msg;
         msg.data={0,0,0,0,0,0};
@@ -174,6 +211,8 @@ class Group {
 };
 
 int main(int argc, char** argv) {
+    fstream file("/home/ros/catkin_ws/src/avoidance/output.csv", ios::out);
+    cout.rdbuf(file.rdbuf());
     ros::init(argc, argv, "avoidance");
     Group group;
 
